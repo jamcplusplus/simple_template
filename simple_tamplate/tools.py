35de4e7a97980bac9d690154687ae74b13a1d049
jamcplusplus <jamcplusplus@gmail.com>
2018-04-19 17:54:10 +0800

    A:增加标签整合方法

--- simple_tamplate/tools.py
+++ simple_tamplate/tools.py
@@ -6,6 +6,8 @@ from cgi import escape
 
 
 class Memory():
+    'zip and unzip in memory'
+
     def __init__(self):
         self.memory = io.BytesIO()
 
@@ -19,10 +21,12 @@ class Memory():
         return out
 
     def unzip_file(self, input_path):
+        'unzip .zip file'
         _z = zipfile.ZipFile(input_path)
         return {name: _z.read(name).decode('utf8') for name in _z.namelist()}
 
     def zip_file(self, static, output_path, extend=None):
+        'zip files'
         zf = zipfile.ZipFile(self.memory, "a", zipfile.ZIP_DEFLATED, False)
         for k, v in static.iteritems():
             zf.writestr(k, v.encode('utf8'))
@@ -30,6 +34,82 @@ class Memory():
             for k, v in extend.iteritems():
                 zf.write(v, k)
         return self.__save(out=output_path)
+
+
+class StripRE(object):
+    'strip broken tag'
+    def __init__(self, xml):
+        self.xml = xml
+
+    def strip_var(self, m):
+        """
+        strip useless tag in vars. Like this:
+        from:
+            `<w:t>{{</w:t>
+            </w:r>
+            <w:r w:rsidR="001A7474">
+                <w:t xml:space="preserve"> i</w:t>
+            </w:r>
+            <w:r>
+                <w:rPr>
+                    <w:rFonts w:hint="eastAsia"/>
+                </w:rPr>
+            <w:t>tem}}</w:t>`
+        to:
+            `<w:t>{{ item}}</w:t>`
+        """
+        txt = m.group()
+        start = m.groups()[0]
+        end = m.groups()[1]
+        wrs = re.findall('<w:t[^>]*>.*?</w:t>', txt)  # tag with text
+        return start + ''.join([re.sub('<[^>]*?>', '', i)
+                                for i in wrs]) + end  # replace tag
+
+    def tag_start(self, name='', reserve=False):
+        if reserve:
+            return r'(<{name}[^>]*?>)'.format(name=name)
+        else:
+            return r'<{name}[^>]*?>'.format(name=name)
+
+    def tag_end(self, name='', reserve=False):
+        if reserve:
+            return r'(</{name}>)'.format(name=name)
+        else:
+            return r'</{name}>'.format(name=name)
+
+    def format_jinja_template(self):
+        REGX_TABLE = [
+            (r"{(" + self.tag_start() + r"[\s\r\n]*)+({|%)", r"{\2"),
+            # join the splited {{ or {%
+            (r"}(" + self.tag_start() + r"[\s\r\n]*)+(%|})", r"}\2"),
+            # replace logic control :
+            (
+                r"(" + self.tag_start("w:t") + ")[^<]*" + "{%" + "[^<}]*?(" +
+                self.tag_end("w:t") + ")"
+                # tag with "{%"
+                r".*?"  # content in {% and %}
+                r"" + self.tag_start("w:t") + "[^<{]*?" + "%}" + "[^<]*" +
+                self.tag_end("w:t") + ""
+                # tag with "%}"
+                ,
+                self.strip_var),
+            # replace vars :
+            (
+                r"(" + self.tag_start("w:t") + ")[^<]*" + "{{" + "[^<}]*?(" +
+                self.tag_end("w:t") + ")"
+                # tag with "{{"
+                r".*?"
+                # content in {{ and }}
+                r"" + self.tag_start("w:t") + "[^<{]*?" + "}}" + "[^<]*" +
+                self.tag_end("w:t") + ""
+                # tag with "}}"
+                ,
+                self.strip_var),
+        ]
+        txt = self.xml
+        for regx, rep in REGX_TABLE:
+            txt = re.sub(regx, rep, txt, flags=re.DOTALL)
+        return txt
 
 
 def safe_string(data):
@@ -49,7 +129,7 @@ def safe_string(data):
         data = escape(data)
     return data
 
+
 def max_rid(xml):
     _re = re.compile(r'rId\d+')
     return int(max(_re.findall(xml)).replace('rId', ''))
-    
